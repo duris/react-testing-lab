6d5298a913b413cde48542118d9fe3487457eca3
Ross <ross@duris.io>
2023-02-03 12:43:02 -0500

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('greeting', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hello John, welcome to Spain!/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('button', () => {
+  render(<App />);
+  const linkElement = screen.getByTestId
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/Greet.js
@@ -0,0 +1,9 @@
+const Greet = ({name, location}) => {
+  return(
+    <div>
+      <h2>Hello {name}, welcome to {location}!</h2>
+    </div>
+  )
+}
+
+export default Greet
